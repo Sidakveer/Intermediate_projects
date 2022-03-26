bf61255796345ae7d188cd7fa82b46fc71f6bad5
Sidakveer <sidakveersingh8@gmail.com>
2022-03-26 14:21:36 -0400

    finish code for quiz_game, add other files

---
+++ quiz_game/main.py
@@ -0,0 +1,15 @@
+from data import question_data
+from question_model import Question, QuizBrain
+
+
+question_bank = []
+for item in question_data:
+    question_bank.append(Question(item["text"], item["answer"]))
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_q():
+
+    quiz.next_q()
+    print(f"You have completed the quiz and your final score is {quiz.score}/{len(question_bank)}")
