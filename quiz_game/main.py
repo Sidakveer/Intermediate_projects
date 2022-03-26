92214083f489d70842a7ddf3f02dc2f800ced38a
Sidakveer <sidakveersingh8@gmail.com>
2022-03-26 16:24:17 -0400

    add code for random walk using the turtle module

--- quiz_game/main.py
+++ quiz_game/main.py
@@ -1,7 +1,6 @@
 from data import question_data
 from question_model import Question, QuizBrain
 
-
 question_bank = []
 for item in question_data:
     question_bank.append(Question(item["text"], item["answer"]))
@@ -13,3 +12,10 @@ while quiz.still_has_q():
 
     quiz.next_q()
     print(f"You have completed the quiz and your final score is {quiz.score}/{len(question_bank)}")
+
+
+
+
+
+
+
