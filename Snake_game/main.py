c25c4d61b053aa64d7772df59559a743785056d0
Sidakveer <sidakveersingh8@gmail.com>
2022-04-02 10:43:53 -0400

    add code to make the snake move on screen without breaking each segment

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -1,9 +1,11 @@
+import time
 from turtle import Screen, Turtle
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 segments = []
 
 
@@ -15,6 +17,20 @@ for x in range(3):
     t.goto(x=a, y=t.ycor())
     a -= 20
     segments.append(t)
+
+
+game_on = True
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    for x in range(len(segments) - 1, 0, -1):
+        newx = segments[x - 1].xcor()
+        newy = segments[x - 1].ycor()
+        segments[x].goto(x=newx, y=newy)
+    segments[0].forward(20)
+
+
+
 
 
 
