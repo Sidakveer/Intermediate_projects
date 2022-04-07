fb5b4c56d253874b5850642b896de55c3f5583cd
Sidakveer <sidakveersingh8@gmail.com>
2022-04-07 18:12:55 -0400

    add scoreboard class to food.py and make changes in main.py to implement the scoreboard class

--- Snake_game/food.py
+++ Snake_game/food.py
@@ -11,6 +11,26 @@ class Food(Turtle):
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("green")
         self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
         x_rand = random.randint(-280, 280)
         y_rand = random.randint(-280, 280)
         self.goto(x=x_rand, y=y_rand)
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.goto(x=self.xcor() - 20, y=280)
+        self.hideturtle()
+        self.update(0)
+
+    def update(self, n):
+        self.clear()
+        self.color("white")
+        self.write(f"Score =  {n}",align="center" , font=(f"arial =", 10, "bold"))
+
+
+
--- Snake_game/main.py
+++ Snake_game/main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen, Turtle
 from snake import Snake
+from food import Food, Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,9 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food1 = Food()
+scoreboard1 = Scoreboard()
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
@@ -16,11 +20,18 @@ screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
 game_on = True
+player_score = 0
 while game_on:
+
     screen.update()
     time.sleep(0.1)
 
     snake.move()
+
+    if snake.head.distance(food1) < 15:
+        food1.refresh()
+        player_score += 1
+        scoreboard1.update(player_score)
 
 
 
