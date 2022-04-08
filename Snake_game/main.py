520c4db28cc73e07784b2385a8390dfed6511176
Sidakveer <sidakveersingh8@gmail.com>
2022-04-08 13:11:54 -0400

    final code for main.py

--- Snake_game/main.py
+++ Snake_game/main.py
@@ -30,14 +30,22 @@ while game_on:
 
     if snake.head.distance(food1) < 15:
         food1.refresh()
+        snake.extend()
         player_score += 1
         scoreboard1.update(player_score)
 
 
+    #detect collision with the wall
+    if snake.head.xcor() > 285 or snake.head.ycor() < -285 or snake.head.xcor() < -285 or snake.head.ycor() > 285:
+        game_on = False
+        scoreboard1.game_over()
 
-
-
-
+    #detect collision with tail
+    for segment in snake.segments[1::]:
+        if snake.head.distance(segment) < 10:
+            game_on = False
+            scoreboard1.game_over()
 
 
 screen.mainloop()
+
